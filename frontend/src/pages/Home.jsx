339ebeaab558e6bd7e0a1452a50e630314566b5b
YBuddhika8saw <2020is018@stu.ucsc.cmb.ac.lk>
2024-02-11 01:38:55 +0530

    add routes in frontend

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+export default function Home() {
+  return (
+    <div>
+      <h1> This is Home </h1>
+    </div>
+  );
+}
