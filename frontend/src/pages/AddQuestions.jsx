83693a8cfc718e1cb7e22c808c01c5cbaad85993
YBuddhika8saw <2020is018@stu.ucsc.cmb.ac.lk>
2024-02-21 16:24:09 +0530

    update add question view

--- frontend/src/pages/AddQuestions.jsx
+++ frontend/src/pages/AddQuestions.jsx
@@ -3,8 +3,8 @@ import { MDBInput, MDBBtn, MDBRange, MDBTextArea } from "mdb-react-ui-kit";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 import { useState } from "react";
-import axios from "axios"; 
-import Swal from 'sweetalert2'
+import axios from "axios";
+import Swal from "sweetalert2";
 
 export default function AddQuestions() {
   const [qText, setQText] = useState("");
@@ -15,58 +15,59 @@ export default function AddQuestions() {
   const [qDifficulty, setqDifficulty] = useState("");
   const [qSpace, setqSpace] = useState("");
 
-
   const submitHandler = async (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  const formData = {
-    qText,
-    qTime,
-    qType,
-    qSubject,
-    qSubjectArea,
-    qDifficulty,
-    qSpace
-  };
+    const formData = {
+      qText,
+      qTime,
+      qType,
+      qSubject,
+      qSubjectArea,
+      qDifficulty,
+      qSpace,
+    };
 
-  try {
-    const response = await axios.post(`http://localhost:5000/api/question/addQuestion`, { formData });
+    try {
+      const response = await axios.post(
+        `http://localhost:5000/api/question/addQuestion`,
+        { formData }
+      );
 
-    // Check if the request was successful
-    if (response.status === 201 && response.data.key === 'success') {
-      // Display success message
-      Swal.fire("Question added successfully");
-      // alert('');
-      // Reset form fields
-      setQText("");
-      setqTime("");
-      setqType("");
-      setqSubject("");
-      setqSubjectArea("");
-      setqDifficulty(1);
-      setqSpace("");
-    } else {
+      // Check if the request was successful
+      if (response.status === 201 && response.data.key === "success") {
+        // Display success message
+        Swal.fire("Question added successfully");
+        // alert('');
+        // Reset form fields
+        setQText("");
+        setqTime("");
+        setqType("");
+        setqSubject("");
+        setqSubjectArea("");
+        setqDifficulty(1);
+        setqSpace("");
+      } else {
+        // Display error message
+        // alert('Failed to add question');
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Failed to add question",
+        });
+      }
+    } catch (error) {
+      // Handle error
+      console.error("Error:", error);
       // Display error message
-      // alert('Failed to add question');
+      // alert('An error occurred while adding the question');
       Swal.fire({
         icon: "error",
         title: "Oops...",
-        text: "Failed to add question",
+        text: "An error occurred while adding the question",
       });
     }
-  } catch (error) {
-    // Handle error
-    console.error('Error:', error);
-    // Display error message
-    // alert('An error occurred while adding the question');
-    Swal.fire({
-      icon: "error",
-      title: "Oops...",
-      text: "An error occurred while adding the question",
-    });
-  }
-};
-
+  };
 
   return (
     <>
@@ -75,7 +76,7 @@ export default function AddQuestions() {
 
       <div className="main">
         <div className="formContainer">
-           <h1> Add Questions </h1>
+          <h1> Add Questions </h1>
           <form onSubmit={submitHandler} method="post">
             <MDBTextArea
               id="qText"
@@ -86,15 +87,22 @@ export default function AddQuestions() {
               onChange={(e) => setQText(e.target.value)}
             />
 
-            <MDBInput wrapperClass="mb-4" id="qTime" label="Question Time" type="number"
-        pattern="[0-9]*" value={qTime} onChange={(e) => setqTime(e.target.value)}/>
+            <MDBInput
+              wrapperClass="mb-4"
+              id="qTime"
+              label="Question Time"
+              type="number"
+              pattern="[0-9]*"
+              value={qTime}
+              onChange={(e) => setqTime(e.target.value)}
+            />
 
             <MDBInput
               wrapperClass="mb-4"
               id="qSpace"
               label="Space Allocation in Inches"
               type="number"
-        pattern="[0-9]*"
+              pattern="[0-9]*"
               value={qSpace}
               onChange={(e) => setqSpace(e.target.value)}
             />
@@ -109,9 +117,21 @@ export default function AddQuestions() {
               value={qDifficulty}
               onChange={(e) => setqDifficulty(e.target.value)}
             />
-            <MDBInput wrapperClass="mb-4" id="qType" label="Question Type" value={qType} onChange={(e) => setqType(e.target.value)} />
+            <MDBInput
+              wrapperClass="mb-4"
+              id="qType"
+              label="Question Type"
+              value={qType}
+              onChange={(e) => setqType(e.target.value)}
+            />
 
-            <MDBInput wrapperClass="mb-4" id="qSubject" label="Subject" value={qSubject} onChange={(e) => setqSubject(e.target.value)} />
+            <MDBInput
+              wrapperClass="mb-4"
+              id="qSubject"
+              label="Subject"
+              value={qSubject}
+              onChange={(e) => setqSubject(e.target.value)}
+            />
 
             <MDBInput
               wrapperClass="mb-4"
