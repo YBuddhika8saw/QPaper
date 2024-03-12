a67400e1e50f6bf5f163625c7511ba8be05d62c0
YBuddhika8saw <2020is018@stu.ucsc.cmb.ac.lk>
2024-03-12 15:07:27 +0530

    create UI for selecting question and getting their Ids

--- frontend/src/pages/SubjectQuestions.jsx
+++ frontend/src/pages/SubjectQuestions.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Button} from "react-bootstrap";
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+import { useNavigate } from "react-router-dom";
 
 export default function SubjectQuestions() {
-
-    
   const params = new URLSearchParams(window.location.search);
   const subject = params.get("subject");
+  const navigate = useNavigate();
   const [questions, setQuestions] = useState([]);
+  const [selectedIds, setSelectedIds] = useState([]);
+
   useEffect(() => {
     axios
       .get(`http://localhost:5000/api/question/getQuestions?subject=${subject}`)
@@ -19,18 +23,48 @@ export default function SubjectQuestions() {
       });
   }, [subject]);
 
+  const handleIdChange = (questionId) => {
+    // Check if the questionId is already selected
+    if (selectedIds.includes(questionId)) {
+      // If selected, remove it from the array
+      setSelectedIds(selectedIds.filter(id => id !== questionId));
+    } else {
+      // If not selected, add it to the array
+      setSelectedIds([...selectedIds, questionId]);
+    }
+  };
+
+  const sendIds = () => {
+    // Redirect to another page with the selectedIds included as a query parameter
+    navigate(
+      `/CreatePaper/GenaratedPaper?subject=${encodeURIComponent(selectedIds)}`
+    );
+  }
+
   return (
     <div>
-        <h1>{subject} Questions</h1>
-        {questions.map((question, index) => {
+      <Navbar />
+      <Sidebar />
+      <div className='genarateQuestionMain'>
+      <h1>{subject} Questions</h1>
+      
+      {questions.map((question, index) => {
         return (
           <div className="questionBox" key={index}>
             <h5>{`${index + 1}. ${question.question_text}`}</h5>
+            <input
+              type="checkbox"
+              value={question.question_id}
+              onChange={() => handleIdChange(question.question_id)}
+            />
           </div>
+          
         );
       })}
+
+      <Button type="Submit" onClick={sendIds}>Create Paper</Button>
+
+      </div>
     </div>
-
-
-  )
+  );
 }
