a9ebf6862bded2f14af362a9e011196a4d907987
YBuddhika8saw <2020is018@stu.ucsc.cmb.ac.lk>
2024-03-21 15:11:19 +0530

    create dashboad frontend

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Sidebar from '../components/Sidebar'
+import React from "react";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import Card from "react-bootstrap/Card";
 
 export default function Dashboard() {
   return (
@@ -8,9 +9,55 @@ export default function Dashboard() {
       <Navbar />
       {/* import sidebar from '../components/Sidebar' */}
       <Sidebar />
-      <div className='main'>
-      <h1> This is Dashboard </h1>
+      <div className="main">
+      <div className="topRow">
+  <Card className="dashboardCard">
+    <Card.Body style={{backgroundColor:"#f5f7fa"}}>
+      <Card.Title style={{ textAlign: "center", fontWeight: "bold", marginTop: "1rem" }}>
+        Total Subjects
+      </Card.Title>
+      <Card.Text style={{ textAlign: "center", fontWeight: "bold" }}>
+        534
+      </Card.Text>
+    </Card.Body>
+  </Card>
+  <Card className="dashboardCard">
+    <Card.Body style={{backgroundColor:"#f5f7fa"}}>
+      <Card.Title style={{ textAlign: "center", fontWeight: "bold", marginTop: "1rem" }}>
+        Total Questions
+      </Card.Title>
+      <Card.Text style={{ textAlign: "center", fontWeight: "bold" }}>
+        534
+      </Card.Text>
+    </Card.Body>
+  </Card>
+  <Card className="dashboardCard">
+    <Card.Body style={{backgroundColor:"#f5f7fa"}}>
+      <Card.Title style={{ textAlign: "center", fontWeight: "bold", marginTop: "1rem" }}>
+        Total Papers
+      </Card.Title>
+      <Card.Text style={{ textAlign: "center", fontWeight: "bold" }}>
+        534
+      </Card.Text>
+    </Card.Body>
+  </Card>
+</div>
+
+<div class="bottomRow">
+  <table class="dashboardTable">
+    <thead>
+      <tr>
+        <th>Subject</th>
+        <th>Questions</th>
+        <th>Papers</th>
+      </tr>
+    </thead>
+    <tbody>
+    </tbody>
+  </table>
+</div>
+
       </div>
     </div>
-  )
+  );
 }
