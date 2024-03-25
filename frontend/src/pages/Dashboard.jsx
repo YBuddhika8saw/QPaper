6296064cfd0a6cbd6672288da1579a2c25529f23
YBuddhika8saw <2020is018@stu.ucsc.cmb.ac.lk>
2024-03-25 16:25:32 +0530

    connect backend and frontend in Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,8 +2,71 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import Card from "react-bootstrap/Card";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function Dashboard() {
+
+  const[subjectInfo,setSubjectInfo]=useState([]);
+  const[questionCount,setQuestionCount]=useState([]);
+  const[paperCount,setPaperCount]=useState([]);
+  const[totalSubjectsCount,setTotalSubjectsCount]=useState([]);
+
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/question/getTotalQuestions`)
+      .then((response) => {
+
+        setQuestionCount(response?.data?.questionsCount);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/question/getTotalSubjects`)
+      .then((response) => {
+
+        setTotalSubjectsCount(response?.data?.subjectCount);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/question/getTotalPapers`)
+      .then((response) => {
+
+        setPaperCount(response?.data?.paperCount);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/paper/getSubjectsInfo`)
+      .then((response) => {
+
+        setSubjectInfo(response?.data?.subjectInfo);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+
+
+
+
   return (
     <div>
       <Navbar />
@@ -17,7 +80,7 @@ export default function Dashboard() {
         Total Subjects
       </Card.Title>
       <Card.Text style={{ textAlign: "center", fontWeight: "bold" }}>
-        534
+        {totalSubjectsCount}
       </Card.Text>
     </Card.Body>
   </Card>
@@ -27,7 +90,7 @@ export default function Dashboard() {
         Total Questions
       </Card.Title>
       <Card.Text style={{ textAlign: "center", fontWeight: "bold" }}>
-        534
+        {questionCount}
       </Card.Text>
     </Card.Body>
   </Card>
@@ -37,7 +100,7 @@ export default function Dashboard() {
         Total Papers
       </Card.Title>
       <Card.Text style={{ textAlign: "center", fontWeight: "bold" }}>
-        534
+        {paperCount}
       </Card.Text>
     </Card.Body>
   </Card>
@@ -53,6 +116,13 @@ export default function Dashboard() {
       </tr>
     </thead>
     <tbody>
+      {subjectInfo.map((subject) => (
+        <tr key={subject.subject}>
+         <td>{subject.subject}</td>
+         <td><Link to={`/QuestionBank/Questions?subject=${subject.subject}`}> {subject.total_questions}</Link></td>
+          <td>{subject.total_papers}</td>  
+        </tr>
+      ))}
     </tbody>
   </table>
 </div>
